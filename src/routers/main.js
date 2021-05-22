470a4c121b562037219380c4c472fa6f7044855e
Dorage-San <baloonflower554@gmail.com>
2021-05-22 19:49:53 +0900

    #1

--- src/routers/main.js
+++ src/routers/main.js
@@ -3,10 +3,11 @@ import { Router } from 'express';
 const router = new Router();
 
 router.get('/', (req, res) =>
-    res.render('index', {
+    res.render('psychotest', {
         contentHeader: '브레이브걸스 테스트',
         contentBody: '내가 제일 좋아하는 브레이브걸스는?',
-        answers: ['어둠이 오리라', '죽음이 다가오리라'],
+        question: '당신은 어떠한 성격인가요?',
+        answers: ['어둠이 오리라', '죽음이 다가오리라', '또또'],
     }),
 );
 router.get('*', (req, res) => {

f06bc279f3a73f4c670832c87d56d8e8d09e22ce
Dorage-San <baloonflower554@gmail.com>
2021-07-25 12:03:39 +0900

    share

--- src/routers/api.js
+++ src/routers/api.js
@@ -7,4 +7,10 @@ router.get('/question/1', getQuestionApi);
 
 router.post('/question/1', postQuestionApi);
 
+router.post('/img', (req, res) => {
+    let fileName =
+        'https://storage.googleapis.com/pwm-res/test/1/share/ig-story_bali.jpg';
+    res.sendFile(fileName);
+});
+
 export default router;
--- static/js/result/index.js
+++ static/js/result/index.js
@@ -1,20 +1,39 @@
 import '../../css/result/index.sass';
-import dtoi from 'dom-to-image';
 
 const event = {
     onClickRetry: () => {
         window.location.href = `${window.location.href.split('/result/')[0]}`;
     },
     onClickSaveImage: async () => {
-        const dataURL = await dtoi.toJpeg(
-            document.getElementsByClassName('.body'),
-            { quality: 1 },
-        );
         const link = document.createElement('a');
         link.download = 'my_image.jpg';
         link.href = dataURL;
         link.click();
     },
+    onClickShareCopyLink: (e) => {
+        const { currentTarget } = e;
+        const input = currentTarget.querySelector('#cpy-link-input');
+        input.style.display = 'block';
+        input.select();
+        document.execCommand('copy');
+        input.style.display = 'none';
+        window.alert('copied link');
+    },
+    onClickSaveIGPost: async (e) => {
+        const { blob } = await fetch(
+            'https://storage.googleapis.com/pwm-res/test/1/share/ig-story_java.jpg',
+        );
+        console.log(blob);
+        const link = document.createElement('a');
+        link.download = 'bali.png';
+        link.href = blob;
+        link.click();
+        link.remove();
+    },
+    onClickSaveIGStory: (e) => {},
+    onClickShareFB: (e) => {},
+    onClickShareTwt: (e) => {},
+    onClickShareWA: (e) => {},
 };
 
 document
@@ -23,3 +42,9 @@ document
 document
     .querySelector('.control__button-save-img')
     .addEventListener('click', event.onClickSaveImage);
+document
+    .getElementById('cpy-link')
+    .addEventListener('click', event.onClickShareCopyLink);
+document
+    .getElementById('sv_ig-post')
+    .addEventListener('click', event.onClickSaveIGPost);
